705e56c7760c37f222c835c3abd727b3c876b98a
MrLiuYS <3050700400@qq.com>
2020-10-24 16:44:40 +0800

    ..

--- wash_data.py
+++ wash_data.py
@@ -12,59 +12,48 @@ import re
 
 import os
 
+
+def traverse(tree,treeArray):
+    for node in tree:
+        if type(node) == bs4.element.Tag:
+            pTree= traverse(node,[])
+            if(len(pTree)==1):
+                treeArray.append(pTree[0])
+            elif len(pTree) > 1:
+                treeArray.append(pTree)
+            
+                
+        elif type(node) == bs4.element.NavigableString:
+            if len(node):
+                treeArray.append(node)
+        else :
+            if len(node):
+                treeArray.append(node)
+    if(len(treeArray)==1):
+        return treeArray[0]
+    else:
+        return treeArray
+
 #1966年1月1日01-03時  (刀砧日)
 def get_top_header_time(soup):
     readme_system_solid = soup.find_all(class_='readme_system_solid')[0]
-    bs = []
-    for row in readme_system_solid.findAll("b"):
-        bs.append(strFormat(row.text))
-    return bs
+    return traverse(readme_system_solid,[])
+    # bs = []
+    # for row in readme_system_solid.findAll("b"):
+    #     bs.append(strFormat(row.text))
+    # return bs
 
 #1月1 庚申日 丑時( 01-03時 )
 def get_top_table_header(soup):
     table_header = soup.table.tr.td.table.tr
 
-    bs = []
-    for row in table_header:
-        if row.name != None :
-            bs.append(strFormat(row.text))
-    return bs
+    # bs = []
+    return traverse(table_header,[])
+    # for row in table_header:
+    #     if row.name != None :
+    #         bs.append(strFormat(row.text))
+    # return bs
 
-# def get_top_table_left(soup):
-#     bs = []
-#     table_header = soup.table.contents[3]
-#     table_header = table_header.table
-#     for index, row in enumerate(table_header):
-#         if (row.name != None) & (5 != index) :
-#             subs=[]
-#             for sub in row:
-#                 if (sub.name != None):
-#                     subs.append(strFormat(sub.text))
-#             bs.append(subs)
-#     return bs
-
-def traverse(tree,treeArray):
-
-    for node in tree:
-        if type(node) == bs4.element.Tag:
-
-            pTree= traverse(node,[])
-            if(len(pTree)):
-                treeArray.append(pTree)
-            
-        elif type(node) == bs4.element.NavigableString:
-            print('-----------------222')
-            if len(node):
-                treeArray.append(node)
-            
-            print('-----------------222')
-        else :
-            print('-----------------333')
-            if len(node):
-                treeArray.append(node)
-            print('-----------------333')
-            # subs.append(strFormat(sub.text))
-    return treeArray
 
 def get_top_table(soup):
     bs = []
@@ -88,38 +77,41 @@ def get_top_table(soup):
 def get_panel_head_nav(soup):
     bs = []
     panelHeadNav = soup.find_all(class_='panelHeadNav')[0]
-    for row in panelHeadNav.findAll("li"):
-        bs.append(strFormat(row.text))
-    return bs
+    return traverse(panelHeadNav,[])
+    # for row in panelHeadNav.findAll("li"):
+    #     bs.append(strFormat(row.text))
+    # return bs
     
 def get_panel1(soup):
     bs = []
     panel1 = soup.find_all(id='panel1')[0]
-    for index, row in enumerate(panel1):
-        if (row.name != None)  :
-            for sub in row:
-                if (sub.name != None):
-                    sub1s=[]
-                    for sub1 in sub:
-                        if (sub1.name != None):
-                            sub1s.append(strFormat(sub1.text))
-                    bs.append(sub1s)
-    return bs
+    return traverse(panel1,[])
+    # for index, row in enumerate(panel1):
+    #     if (row.name != None)  :
+    #         for sub in row:
+    #             if (sub.name != None):
+    #                 sub1s=[]
+    #                 for sub1 in sub:
+    #                     if (sub1.name != None):
+    #                         sub1s.append(strFormat(sub1.text))
+    #                 bs.append(sub1s)
+    # return bs
 
 def get_panel2(soup):
     bs = []
     panel2 = soup.find_all(id='panel2')[0]
-    for index, row in enumerate(panel2):
-        if (row.name != None)  :
-            subs=[]
-            for sub in row:
-                if type(sub) == bs4.element.NavigableString:
-                    subs.append(strFormat(sub))
-                else :
-                    subs.append(strFormat(sub.text))
-            bs.append(subs)
+    return traverse(panel2,[])
+    # for index, row in enumerate(panel2):
+    #     if (row.name != None)  :
+    #         subs=[]
+    #         for sub in row:
+    #             if type(sub) == bs4.element.NavigableString:
+    #                 subs.append(strFormat(sub))
+    #             else :
+    #                 subs.append(strFormat(sub.text))
+    #         bs.append(subs)
 
-    return bs
+    # return bs
 
 def get_panel3(soup):
     bs = []
@@ -154,65 +146,69 @@ def get_panel3_columnar(soup):
 def get_panel4(soup):
     bs = []
     panel2 = soup.find_all(id='panel4')[0]
-    for index, row in enumerate(panel2):
-        if (row.name != None)  :
-            subs=[]
-            for sub in row:
-                if type(sub) == bs4.element.NavigableString:
-                    subs.append(strFormat(sub))
-                else :
-                    subs.append(strFormat(sub.text))
-            bs.append(subs)
+    return traverse(panel2,bs)
+    # for index, row in enumerate(panel2):
+    #     if (row.name != None)  :
+    #         subs=[]
+    #         for sub in row:
+    #             if type(sub) == bs4.element.NavigableString:
+    #                 subs.append(strFormat(sub))
+    #             else :
+    #                 subs.append(strFormat(sub.text))
+    #         bs.append(subs)
 
-    return bs
+    # return bs
 
 #趨吉避凶
 def get_panel5(soup):
     bs = []
     panel2 = soup.find_all(id='panel5')[0]
-    for index, row in enumerate(panel2):
-        if (row.name != None)  :
-            subs=[]
-            for sub in row:
-                if type(sub) == bs4.element.NavigableString:
-                    subs.append(strFormat(sub))
-                else :
-                    subs.append(strFormat(sub.text))
-            bs.append(subs)
+    return traverse(panel2,bs)
+    # for index, row in enumerate(panel2):
+    #     if (row.name != None)  :
+    #         subs=[]
+    #         for sub in row:
+    #             if type(sub) == bs4.element.NavigableString:
+    #                 subs.append(strFormat(sub))
+    #             else :
+    #                 subs.append(strFormat(sub.text))
+    #         bs.append(subs)
 
-    return bs
+    # return bs
 
 #秤骨論命
 def get_panel6(soup):
     bs = []
     panel2 = soup.find_all(id='panel6')[0]
-    for index, row in enumerate(panel2):
-        if (row.name != None)  :
-            subs=[]
-            for sub in row:
-                if type(sub) == bs4.element.NavigableString:
-                    subs.append(strFormat(sub))
-                else :
-                    subs.append(strFormat(sub.text))
-            bs.append(subs)
+    return traverse(panel2,bs)
+    # for index, row in enumerate(panel2):
+    #     if (row.name != None)  :
+    #         subs=[]
+    #         for sub in row:
+    #             if type(sub) == bs4.element.NavigableString:
+    #                 subs.append(strFormat(sub))
+    #             else :
+    #                 subs.append(strFormat(sub.text))
+    #         bs.append(subs)
 
-    return bs
+    # return bs
 
 #生肖論命
 def get_panel7(soup):
     bs = []
     panel2 = soup.find_all(id='panel7')[0]
-    for index, row in enumerate(panel2):
-        if (row.name != None)  :
-            subs=[]
-            for sub in row:
-                if type(sub) == bs4.element.NavigableString:
-                    subs.append(strFormat(sub))
-                else :
-                    subs.append(strFormat(sub.text))
-            bs.append(subs)
+    return traverse(panel2,bs)
+    # for index, row in enumerate(panel2):
+    #     if (row.name != None)  :
+    #         subs=[]
+    #         for sub in row:
+    #             if type(sub) == bs4.element.NavigableString:
+    #                 subs.append(strFormat(sub))
+    #             else :
+    #                 subs.append(strFormat(sub.text))
+    #         bs.append(subs)
 
-    return bs
+    # return bs
 
 #生肖
 def get_panel7_zodiac(soup):
